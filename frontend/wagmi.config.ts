e6be4998605b54d08b419f4327c1cae3c2e4479d
rsproule <rsproule23@gmail.com>
2023-06-23 16:19:24 -0700

    feat: point to goerli

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         TankGame: {
           [chains.mainnet.id]: "0x021dbff4a864aa25c51f0ad2cd73266fde66199d",
           [chains.foundry.id]: "0x95401dc811bb5740090279ba06cfa8fcf6113778",
+          [chains.goerli.id]: "0x740d2B7c7f24DdB939FD108e8B8ba96E2187e861",
         },
       },
       project: "../contracts",
