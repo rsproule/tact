56b17b37da403142fc7a47d72c5fab9c9994f2b3
rsproule <rsproule23@gmail.com>
2023-06-23 16:01:28 -0700

    chore: remove unused boilerplate

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Connected } from "../components/wagmi/Connected";
 import { TankGame } from "../components/tankGame/TankGame";
 import { EventStream } from "../components/tankGame/EventsStream";
 import { Toaster } from "../components/ui/toaster";
-export function Page() {
+export default function HomePage() {
   return (
     <div className="grid container mx-auto border dark:bg-slate-800">
       <ConnectKitButton showBalance />
@@ -16,5 +16,3 @@ export function Page() {
     </div>
   );
 }
-
-export default Page;
