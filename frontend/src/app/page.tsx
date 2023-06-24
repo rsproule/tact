78fe1e8e8529e3c9349fe17b738140c6ef9f9efa
rsproule <rsproule23@gmail.com>
2023-06-23 17:11:18 -0700

    chore: some ui clean up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,13 +6,15 @@ import { Toaster } from "../components/ui/toaster";
 import Rules from "../components/tankGame/Rules";
 export default function HomePage() {
   return (
-    <div className="grid container mx-auto border dark:bg-slate-800">
+    <div className="grid container mx-auto p-4">
       <ConnectKitButton showBalance />
-
+      <div className="flex justify-center">
+        <h1 className="text-4xl">Tank Turn Tactics (beta)</h1>
+      </div>
       <Connected>
+        <Rules />
         <TankGame />
         <EventStream />
-        <Rules/>
         <Toaster />
       </Connected>
     </div>
