ebf9a752b259ce0137be2fc6dd862f3c06d15f33
rsproule <rsproule23@gmail.com>
2023-06-26 16:06:58 -0700

    feat: name the tanks for the event stream

--- frontend/src/components/tankGame/Tank.tsx
+++ frontend/src/components/tankGame/Tank.tsx
@@ -17,6 +17,8 @@ const OWNERS: Map<String, String> = new Map<string, string>([
   ["0x5CE306109b8de8d001d52F2140383A54AB55CdB2", "Jonah"],
   ["0xDC40CbF86727093c52582405703e5b97D5C64B66", "Mason"],
   ["0x2CB8636240693B445ac98F2091b58A898e35e60B", "Joe//Osprey"],
+  // ["", "Will"],
+  // ["", "Sterling"],
 ]);
 export function Tank({ tank, tankId }: typeof ITank) {
   let { address } = useAccount();
