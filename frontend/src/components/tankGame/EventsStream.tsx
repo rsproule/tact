0c1225e8cf40da48b181c2d9eeee97f7961ab8bd
rsproule <rsproule23@gmail.com>
2023-06-26 16:06:58 -0700

    feat: name the tanks for the event stream

--- frontend/src/components/tankGame/EventsStream.tsx
+++ frontend/src/components/tankGame/EventsStream.tsx
@@ -14,12 +14,31 @@ import { useState } from "react";
 import { formatEther } from "viem";
 import { Card, CardHeader } from "../ui/card";
 
+const TANK_MAPPING = [
+  "Ryan",
+  "Jonah",
+  "Sam",
+  "Mason",
+  "Spencer",
+  "Yigit",
+  "Caleb",
+  "Joe",
+];
+
+const toTankName = (tankId: bigint | undefined) => {
+  if (tankId === undefined) {
+    return "Tank " + tankId!.toString();
+  }
+  return TANK_MAPPING[Number(tankId!)];
+};
 export function EventStream() {
   const [events, setEvents] = useState<string[]>([]);
   useTankGameMoveEvent({
     listener: (e) => {
       e.map((event) => {
-        let moveString = `Tank ${event.args.tankId} moved to (${event.args.x}, ${event.args.y})`;
+        let moveString = `${toTankName(event.args.tankId)} moved to (${
+          event.args.x
+        }, ${event.args.y})`;
         setEvents((prev) => {
           return [...prev, moveString];
         });
@@ -29,7 +48,7 @@ export function EventStream() {
   useTankGameShootEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${event.args.tankId} shot ${event.args.targetId}`;
+        let s = `${toTankName(event.args.tankId)} shot ${event.args.targetId}`;
         setEvents((prev) => {
           return [...prev, s];
         });
@@ -39,9 +58,11 @@ export function EventStream() {
   useTankGameGiveEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${event.args.fromId} gave ${
+        let s = `${toTankName(event.args.fromId)} gave ${
           event.args.hearts || event.args.aps
-        } ${event.args.hearts ? "hearts" : "aps"} to ${event.args.toId}`;
+        } ${event.args.hearts ? "hearts" : "aps"} to ${toTankName(
+          event.args.toId
+        )}`;
         setEvents((prev) => {
           return [...prev, s];
         });
@@ -51,7 +72,9 @@ export function EventStream() {
   useTankGameUpgradeEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${event.args.tankId} upgraded range to ${event.args.range} `;
+        let s = `${toTankName(event.args.tankId)} upgraded range to ${
+          event.args.range
+        } `;
         setEvents((prev) => {
           return [...prev, s];
         });
@@ -61,7 +84,9 @@ export function EventStream() {
   useTankGameVoteEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${event.args.voter} voted to curse ${event.args.cursed} during epoch ${event.args.epoch}`;
+        let s = `${toTankName(event.args.voter)} voted to curse ${
+          event.args.cursed
+        } during epoch ${event.args.epoch}`;
         setEvents((prev) => {
           return [...prev, s];
         });
@@ -71,7 +96,9 @@ export function EventStream() {
   useTankGameDripEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${event.args.tankId} claimed ${event.args.amount} action points from the faucet.`;
+        let s = `${toTankName(event.args.tankId)} claimed ${
+          event.args.amount
+        } action points from the faucet.`;
         setEvents((prev) => {
           return [...prev, s];
         });
@@ -81,9 +108,9 @@ export function EventStream() {
   useTankGameClaimEvent({
     listener(e) {
       e.map((event) => {
-        let s = `Tank ${
+        let s = `${toTankName(
           event.args.tankId
-        } claimed their winnings of ${formatEther(
+        )} claimed their winnings of ${formatEther(
           event.args.amount!
         )} ether and sent it to ${event.args.reciever} .`;
         setEvents((prev) => {
