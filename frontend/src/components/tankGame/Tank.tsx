2abad6284228121bfbedeadb1b36a66cbbc3ea70
rsproule <rsproule23@gmail.com>
2023-06-26 13:10:56 -0700

    feat: add the mapping of address to username

--- frontend/src/components/tankGame/Tank.tsx
+++ frontend/src/components/tankGame/Tank.tsx
@@ -8,6 +8,15 @@ import {
 import { Heart, Target, User, Zap } from "lucide-react";
 import { ITank } from "./ITank";
 
+const OWNERS: Map<String, String> = new Map<string, string>([
+  ["0x5337122c6b5ce24D970Ce771510D22Aeaf038C44", "Ryan"],
+  ["0x425374B4da782f0ec5DeC7b45B0C1e08dc6B5A65", "Caleb"],
+  ["0x2a233E71Ba12fE39E587D402Acf4E41e8B008e31", "Spencer"],
+  ["0x859B7FA9b70746720bdeA1453D04161F3C829684", "Yigit"],
+  ["0xb7430de9B4D8e5cDB951019d7651cD5fda630498", "Sam"],
+  ["0x5CE306109b8de8d001d52F2140383A54AB55CdB2", "Jonah"],
+  ["0xDC40CbF86727093c52582405703e5b97D5C64B66", "Mason"],
+]);
 export function Tank({ tank, tankId }: typeof ITank) {
   let { address } = useAccount();
   return (
@@ -33,6 +42,12 @@ export function Tank({ tank, tankId }: typeof ITank) {
               <div className="flex items-center pt-2">
                 <User className="mr-2 h-4 w-4 " />{" "}
                 <span className="text-xs">{tank.owner.toString()}</span>
+              </div>
+              <div className="flex items-center pt-2">
+                <User className="mr-2 h-4 w-4 " />
+                <span className="text-xs">
+                  {OWNERS.get(tank.owner.toString())}
+                </span>
               </div>
               <div className="flex items-center pt-2">
                 <Heart className="mr-2 h-4 w-4 " />{" "}
