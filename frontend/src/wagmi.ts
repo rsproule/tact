3cc795bbcd84259155eaa415a32dee0ae2f4859c
rsproule <rsproule23@gmail.com>
2023-06-26 23:05:33 -0700

    fix: public env vars

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,8 +1,8 @@
 import { getDefaultConfig } from "connectkit";
 import { goerli } from "viem/chains";
 import { createConfig } from "wagmi";
-const walletConnectProjectId = process.env.WALLET_CONNECT_APP!;
-const alchemyId = process.env.ALCHEMY_ID!;
+const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_APP!;
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
 
 export const config = createConfig(
   getDefaultConfig({
