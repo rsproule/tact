f42026abce2e7af3f43ff70e432cc00128447921
rsproule <rsproule23@gmail.com>
2023-06-27 19:29:20 -0700

    feat: highlight the tanks range

--- frontend/src/components/tankGame/Square.tsx
+++ frontend/src/components/tankGame/Square.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { Tank } from "./Tank";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { ITank } from "./ITank";
 import EmptySquareMenu from "./actions/EmptySquareMenu";
 import EnemySquareMenu from "./actions/EnemySquareMenu";
@@ -15,17 +12,27 @@ interface SquareProps {
   boardSize: number;
   tank: typeof ITank | undefined;
   ownersTankId: bigint | undefined;
+  isShootRange: boolean;
+  isMoveRange: boolean;
+  onClick: () => void;
 }
 export function Square(props: SquareProps) {
   let [open, setOpen] = useState(false);
   return (
     <div>
-      <DropdownMenu onOpenChange={(o) => setOpen(o)}>
+      <DropdownMenu
+        onOpenChange={(o) => {
+          setOpen(o);
+          props.onClick();
+        }}
+      >
         <DropdownMenuTrigger asChild>
           <div
-            className={`border w-full h-0 shadow-sm aspect-w-1 aspect-h-1 rounded-sm ${
-              open ? "bg-green-200" : "bg-gray-600"
-            }`}
+            className={`border w-full h-0 shadow-sm aspect-w-1 aspect-h-1 rounded-sm ${getColor(
+              open,
+              props.isShootRange,
+              props.isMoveRange
+            )}`}
           >
             {props.tank && (
               <Tank
@@ -59,3 +66,18 @@ export function Square(props: SquareProps) {
   );
 }
 
+function getColor(selected: boolean, shootRange: boolean, moveRange: boolean) {
+  if (selected) {
+    return "bg-green-200";
+  }
+  if (shootRange && moveRange) {
+    return "bg-orange-200";
+  }
+  if (shootRange) {
+    return "bg-red-200";
+  }
+  if (moveRange) {
+    return "bg-gray-200";
+  }
+  return "bg-gray-500";
+}
