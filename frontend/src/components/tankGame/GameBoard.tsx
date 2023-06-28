634b549e664933ef1f8ef0ea6cf8bc2040fe837a
rsproule <rsproule23@gmail.com>
2023-06-27 19:29:20 -0700

    feat: highlight the tanks range

--- frontend/src/components/tankGame/GameBoard.tsx
+++ frontend/src/components/tankGame/GameBoard.tsx
@@ -1,8 +1,10 @@
 import { Square } from "./Square";
 import { useTankGameGetAllTanks, useTankGamePlayers } from "@/src/generated";
+import { useState } from "react";
 import { useAccount } from "wagmi";
 
 export function Board({ boardSize }: { boardSize: bigint | undefined }) {
+  const [selectedTank, setSelectedTank] = useState<any>();
   let tanks = useTankGameGetAllTanks({ watch: true });
   const { address } = useAccount();
   let ownersTankId = useTankGamePlayers({
@@ -14,39 +16,67 @@ export function Board({ boardSize }: { boardSize: bigint | undefined }) {
   }
 
   const a = new Array(Number(boardSize + BigInt(1))).fill(0);
-  return boardSize === BigInt(0) ? (
-    <div></div>
-  ) : (
-    <div className={`grid grid-cols-41`}>
-      {a.map((x: bigint, i: number) =>
-        a.map((y: bigint, j: number) => {
-          const tank = tanks.data?.find((tank) => {
+  return (
+    boardSize !== BigInt(0) && (
+      <div className={`grid grid-cols-41`}>
+        {a.map((x: bigint, i: number) =>
+          a.map((y: bigint, j: number) => {
+            const tank = tanks.data?.find((tank) => {
+              return (
+                tank.position.x === BigInt(i) && tank.position.y === BigInt(j)
+              );
+            });
             return (
-              tank.position.x === BigInt(i) && tank.position.y === BigInt(j)
+              <div key={`${i},${j}`}>
+                {BigInt(i) === boardSize && BigInt(j) !== boardSize && (
+                  <div>{j}</div>
+                )}
+                {BigInt(j) === boardSize && BigInt(i) !== boardSize && (
+                  <div>{i}</div>
+                )}
+                {BigInt(i) !== boardSize && BigInt(j) !== boardSize && (
+                  <Square
+                    onClick={() => {
+                      setSelectedTank(tank);
+                    }}
+                    x={i}
+                    y={j}
+                    boardSize={Number(boardSize)}
+                    tank={tank}
+                    ownersTankId={ownersTankId.data}
+                    isShootRange={
+                      !!selectedTank &&
+                      selectedTank.tank.hearts > 0 &&
+                      getDistance(
+                        BigInt(i),
+                        BigInt(j),
+                        selectedTank.position.x,
+                        selectedTank.position.y
+                      ) <= selectedTank.tank.range
+                    }
+                    isMoveRange={
+                      !!selectedTank &&
+                      selectedTank.tank.hearts > 0 &&
+                      getDistance(
+                        BigInt(i),
+                        BigInt(j),
+                        selectedTank.position.x,
+                        selectedTank.position.y
+                      ) <= selectedTank.tank.aps
+                    }
+                  />
+                )}
+              </div>
             );
-          });
-          return (
-            <div key={`${i},${j}`}>
-              {BigInt(i) === boardSize && BigInt(j) !== boardSize && (
-                <div>{j}</div>
-              )}
-              {BigInt(j) === boardSize && BigInt(i) !== boardSize && (
-                <div>{i}</div>
-              )}
-              {/* {j === 0 && <div className="grow-0">{i}</div>} */}
-              {BigInt(i) !== boardSize && BigInt(j) !== boardSize && (
-                <Square
-                  x={i}
-                  y={j}
-                  boardSize={Number(boardSize)}
-                  tank={tank}
-                  ownersTankId={ownersTankId.data}
-                />
-              )}
-            </div>
-          );
-        })
-      )}
-    </div>
+          })
+        )}
+      </div>
+    )
   );
+}
+
+function getDistance(ax: bigint, ay: bigint, bx: bigint, by: bigint) {
+  const x = ax > bx ? ax - bx : bx - ax;
+  const y = ay > by ? ay - by : by - ay;
+  return x > y ? x : y;
 }
