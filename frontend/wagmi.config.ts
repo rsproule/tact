c7d3d2851c2d1384f226371de13958458a80d715
Ryan Sproule <rsproule23@gmail.com>
2023-07-02 11:01:33 -0700

    fix: update frontend to point at the redeployed contract (#24)
    * fix: update frontend to point at the redeployed contract
    
    * fix: script to update the state via the init method and pull data from the inital canonical contract
    
    * fix: update the contracts to support emergency surgury
    
    * chore: remove forge-tst from prod contract and some labelling in the podium

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
         TankGame: {
           [chains.mainnet.id]: "0x021dbff4a864aa25c51f0ad2cd73266fde66199d",
           [chains.foundry.id]: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-          [chains.goerli.id]: "0x1D738bb3c3D594E248Fdb5234b7Af7a2Ecb7B64D",
+          // [chains.goerli.id]: "0x1D738bb3c3D594E248Fdb5234b7Af7a2Ecb7B64D",
+          [chains.goerli.id]: "0x0a8628a32f0AC3A208B8CEf032B38fF08bB140D7",
         },
       },
       project: "../contracts",
