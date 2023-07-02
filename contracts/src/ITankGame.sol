e1fd023e3154765c94cbecb2c13deea67e61f762
Ryan Sproule <rsproule23@gmail.com>
2023-07-02 11:01:33 -0700

    fix: update frontend to point at the redeployed contract (#24)
    * fix: update frontend to point at the redeployed contract
    
    * fix: script to update the state via the init method and pull data from the inital canonical contract
    
    * fix: update the contracts to support emergency surgury
    
    * chore: remove forge-tst from prod contract and some labelling in the podium

--- contracts/src/ITankGame.sol
+++ contracts/src/ITankGame.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.13;
 
 interface ITankGame {
     struct Point {
-        uint x;
-        uint y;
+        uint256 x;
+        uint256 y;
     }
 
     struct GameSettings {
-        uint playerCount;
-        uint boardSize;
-        uint initAPs;
-        uint initHearts;
-        uint voteThreshold;
-        uint initShootRange;
-        uint upgradeCost;
-        uint epochSeconds;
-        uint actionDelaySeconds;
+        uint256 playerCount;
+        uint256 boardSize;
+        uint256 initAPs;
+        uint256 initHearts;
+        uint256 voteThreshold;
+        uint256 initShootRange;
+        uint256 upgradeCost;
+        uint256 epochSeconds;
+        uint256 actionDelaySeconds;
     }
 
     enum Resource {
@@ -24,29 +24,27 @@ interface ITankGame {
         Heart
     }
 
-    function init() external payable;
-
     function join() external payable;
 
-    function move(uint fromId, Point calldata to) external;
+    function move(uint256 fromId, Point calldata to) external;
 
-    function shoot(uint fromId, uint toId) external;
+    function shoot(uint256 fromId, uint256 toId) external;
 
     function give(
-        uint fromId,
-        uint toId,
-        uint hearts,
-        uint aps
+        uint256 fromId,
+        uint256 toId,
+        uint256 hearts,
+        uint256 aps
     ) external;
 
-    function drip(uint tankId) external;
+    function drip(uint256 tankId) external;
 
-    function upgrade(uint tankId) external;
+    function upgrade(uint256 tankId) external;
 
     // dead man actions
-    function vote(uint voter, uint cursed) external;
+    function vote(uint256 voter, uint256 cursed) external;
 
-    function claim(uint tankId, address claimer) external;
+    function claim(uint256 tankId, address claimer) external;
 
     // game settings
     function settings() external view returns (GameSettings memory);
