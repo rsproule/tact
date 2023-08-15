d403f70822699aca8e8dee915691fd0128153b57
rsproule <rsproule23@gmail.com>
2023-08-15 16:20:26 +0200

    chore: format and set board size to 30

--- contracts/src/base/HexBoard.sol
+++ contracts/src/base/HexBoard.sol
@@ -60,11 +60,9 @@ contract HexBoard is Board {
         uint256 i = 1;
         uint256 boardSize = boardSize;
         do {
-            q = seed % (2 * boardSize + 1); // 0 to 60
-            // r is in range [boardSize - q, 2*boardSize] [90-q, 60]
+            q = seed % (2 * boardSize + 1); 
             uint256 minR = q <= boardSize ? boardSize - q : 0;
             uint256 maxR = 3 * boardSize - q;
-            // q+r must be > 30 and < 90
             r = uint256(keccak256(abi.encodePacked(seed))) % (maxR - minR) + minR; // 0 to 60
             s = 3 * boardSize - q - r;
             seed = uint256(keccak256(abi.encodePacked(seed, i)));
