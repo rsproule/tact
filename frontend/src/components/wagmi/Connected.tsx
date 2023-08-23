668db903d06e51c83aee40803f7fccb62f06d2eb
rsproule <rsproule23@gmail.com>
2023-08-23 17:25:15 +0200

    chore: add the rules to the landing page if not connected

--- frontend/src/components/wagmi/Connected.tsx
+++ frontend/src/components/wagmi/Connected.tsx
@@ -1,24 +1,11 @@
 "use client";
 
 import { useAccount } from "wagmi";
+import Rules from "../tankGame/Rules";
 
 export function Connected({ children }: { children: React.ReactNode }) {
   const { isConnected } = useAccount();
 
-  if (!isConnected)
-    return (
-      <div className="flex justify-center">
-        <span className="pr-1">{`
-        Connect Wallet to play. I promise this isn't a rug (it's also just on
-        goerli for now). Code is here: 
-        `}</span>{" "}
-        <a
-          className="text-blue-600"
-          href="https://github.com/rsproule/tanks"
-        >
-          https://github.com/rsproule/tanks
-        </a>
-      </div>
-    );
+  if (!isConnected) return <Rules />;
   return <>{children}</>;
 }
