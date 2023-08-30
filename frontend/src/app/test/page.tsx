1f93c8ea938fab6e4e0d5d8affc56f01b86535be
rsproule <rsproule23@gmail.com>
2023-08-30 15:29:41 -0700

    fix: handle checksum addresses

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -2,14 +2,20 @@
 import { useAccount } from "wagmi";
 import * as tree from "public/tree.json";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import { getAddress } from "viem";
 
 export default function TestJoinPage() {
   const merkleTree = StandardMerkleTree.load(tree as any);
   const { address } = useAccount();
-  const value = tree.values.find((x) => x.value[0] === address);
+  const value = tree.values.find((x) => getAddress(x.value[0]) === address);
+  const proof =
+    value &&
+    merkleTree
+      .getProof(value.value)
+      .map((x) => Object.freeze(x) as `0x${string}`);
   return (
     <div className="container mt-10">
-      {value ? (
+      {proof ? (
         <div className="bg-lime-900 p-10 text-center">
           Found <b>{value.value[1]}</b>, you are ready to go. Good luck,
           soldier.
