fe389617e629ade1309c5e8bd478d1d22b67f954
rsproule <rsproule23@gmail.com>
2023-08-30 11:24:20 -0700

    feat: manifesto until connected

--- frontend/src/app/rules/page.tsx
+++ frontend/src/app/rules/page.tsx
@@ -1,9 +1,7 @@
-import Manifesto from "@/src/components/tankGame/Manifesto";
 import Rules from "../../components/tankGame/Rules";
 export default function RulesPage() {
   return (
     <div className="grid container">
-      <Manifesto />
       <Rules />
     </div>
   );
