5735544ea8e6ef3fe23ba74033ca3a3587c79473
rsproule <rsproule23@gmail.com>
2023-08-30 12:19:59 -0700

    feat: switch to goerli

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,5 +1,5 @@
 import { getDefaultConfig } from "connectkit";
-import { foundry, goerli } from "viem/chains";
+import { goerli } from "viem/chains";
 import { createConfig } from "wagmi";
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_APP!;
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
@@ -7,9 +7,9 @@ const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
 export const config = createConfig(
   getDefaultConfig({
     autoConnect: true,
-    appName: "Tank Trust Tactics",
+    appName: "Tact",
     walletConnectProjectId,
     alchemyId: alchemyId,
-    chains: [foundry],
+    chains: [goerli],
   })
 );
