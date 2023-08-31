06028095520bf4fb6d9b293a8613e48dceb9b295
rsproule <rsproule23@gmail.com>
2023-08-31 16:45:14 -0700

    fix: title pretty

--- frontend/src/components/tankGame/Title.tsx
+++ frontend/src/components/tankGame/Title.tsx
@@ -1,7 +1,10 @@
 export default function Title() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center py-3 items-center">
       <h1 className="text-4xl">Tact</h1>
+      <div className="text-xs ml-4 text-slate-400">
+        A game of trust in a trustless environment
+      </div>
     </div>
   );
 }
