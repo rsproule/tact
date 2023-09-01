fccfceca8f9d40a14fb8e58a9ff14c3d09f49b49
Ryan Sproule <rsproule23@gmail.com>
2023-08-31 18:08:26 -0700

    V2 board fix (#33)
    * sneaky hide some stuff
    
    * fix: all the links filled now

--- frontend/src/components/tankGame/Title.tsx
+++ frontend/src/components/tankGame/Title.tsx
@@ -1,7 +1,11 @@
 export default function Title() {
   return (
     <div className="flex justify-center py-3 items-center">
-      <h1 className="text-4xl">Tact</h1>
+      <h1 className="text-4xl">
+        <a className="text-white hover:text-gray-400" href="/">
+          Tact
+        </a>
+      </h1>
       <div className="text-xs ml-4 text-slate-400">
         A game of trust in a trustless environment
       </div>
