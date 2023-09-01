511d5a1078618294148bfe08150c0aba62cc5d87
rsproule <rsproule23@gmail.com>
2023-08-31 17:16:21 -0700

    feat: update the player list

--- contracts/script/merkle.ts
+++ contracts/script/merkle.ts
@@ -23,6 +23,18 @@ const values = [
   [getAddress("0x72Edd28063647BE9E87F6Df3f4de7138A1a5ceBA"), "will"],
   [getAddress("0xB3c296170c57A7510Bb95EF2E9C47977bC2FF1c8"), "caleb"],
   [getAddress("0xDA744DaCea631029430FD63D83B26F757E054Cb7"), "brian"],
+  [getAddress("0x25C9745e0BA56fa7E40F9F8f61b7Fb49016cf342"), "phil"],
+  [getAddress("0x9B6A3e3A2C81464f873e54C07Ed947dd00938802"), "bot0"],
+  [getAddress("0xf940C90fb67A3992d55B676746669921457997e4"), "bot1"],
+  [getAddress("0xbf69051850247fdc1a8A27c3AD4F7951CE6977b1"), "bot2"],
+  [getAddress("0x7950787FE58E6cd7cB902773E8868c7F6F89d8eD"), "bot3"],
+  [getAddress("0xA16110c3dC7eEF79806C917ed0Bd7fA497c8580b"), "bot4"],
+  [getAddress("0xa192204d0E14233c5c09bB172CE38c2d2d7DDf1B"), "bot5"],
+  [getAddress("0xCc110bd48b55D483B19a4dd378B066dA6c3cD1fE"), "bot6"],
+  [getAddress("0x7219cA1F7d8045c5c757BaC01d6eC81D43504072"), "bot7"],
+  [getAddress("0x60848AFA93C83cA3eA7734E47c3dA155eB78B59B"), "bot8"],
+  [getAddress("0xe54B2534F74b857246Bd5854551F1178aa0BeA71"), "bot9"],
+  [getAddress("0x0541a3Aa3FA999EFE2596324bfeE79ACf11269Df"), "bot10"],
   // ["", "jonah"],
 ];
 
