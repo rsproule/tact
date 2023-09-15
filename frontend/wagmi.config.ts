0a009d81cdb522d3e58f240ec7cdb6b8729ecb19
rsproule <rsproule23@gmail.com>
2023-09-15 11:03:22 -0700

    feat: beta-3 deployment

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -10,20 +10,20 @@ export default defineConfig({
         ITankGame: {
           [chains.mainnet.id]: "0x021dbff4a864aa25c51f0ad2cd73266fde66199d",
           [chains.foundry.id]: "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be",
-          [chains.goerli.id]: "0x460aA36C5f0A15483C114E48783c1354281b5780",
+          [chains.goerli.id]: "0x692E97d2F99e0D595b1349a07a4733a3F5102E94",
         },
         TankGame: {
           [chains.mainnet.id]: "0x021dbff4a864aa25c51f0ad2cd73266fde66199d",
           [chains.foundry.id]: "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be",
-          [chains.goerli.id]: "0x460aA36C5f0A15483C114E48783c1354281b5780",
+          [chains.goerli.id]: "0x692E97d2F99e0D595b1349a07a4733a3F5102E94",
         },
         GameView: {
           [chains.foundry.id]: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-          [chains.goerli.id]: "0xF992766F56cb1395A3584B73f1eb99149fb57f31",
+          [chains.goerli.id]: "0x48E419cA3209e7318679cCFAf3D1c090fbEb7b83",
         },
         TankGameFactory: {
           [chains.foundry.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-          [chains.goerli.id]: "0xDB9a8A333f9c56c935d0a6328935bA5CB46D438F",
+          [chains.goerli.id]: "0x26d0DadeAfeF0AefF3f2CcE09840830146E86A67",
         },
       },
       project: "../contracts",
