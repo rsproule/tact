f8352d58d425eca547f3241ced58e89a628a653d
rsproule <rsproule23@gmail.com>
2023-10-05 11:15:27 -0700

    feat: server testing script, not sure about format of sim data to return

--- server/routes/logs.ts
+++ server/routes/logs.ts
@@ -1,30 +1,39 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { tankGameABI, tankGameAddress } from "../../frontend/src/generated";
+import { tankGameABI } from "../../frontend/src/generated";
 import publicClient from "../api/client";
 var logsRouter = Router();
 
 logsRouter.post("/", logHandler);
 
 function logHandler(req: Request, res: Response, next: NextFunction) {
-  getLogs(req.body.fromBlock).then((logs) => {
-    res.send(
-      // kinda hacky to handle the fact that BigInts are not JSON serializable
-      JSON.parse(
-        JSON.stringify(logs, (key, value) =>
-          typeof value === "bigint" ? value.toString() : value
+  if (!req.body.address) {
+    return res.status(400).send("address not provided");
+  }
+  getLogs(
+    req.body.fromBlock !== undefined ? req.body.fromBlock : 0,
+    req.body.address
+  )
+    .then((logs) => {
+      res.send(
+        // kinda hacky to handle the fact that BigInts are not JSON serializable
+        JSON.parse(
+          JSON.stringify(logs, (key, value) =>
+            typeof value === "bigint" ? value.toString() : value
+          )
         )
-      )
-    );
-  });
+      );
+    })
+    .catch((err) => {
+      return res.status(500).send(err.message);
+    });
 }
 
-async function getLogs(fromBlock: number) {
-  const chainId = publicClient.chain?.id;
+async function getLogs(fromBlock: number, address: `0x${string}`) {
   const filter = await publicClient.createContractEventFilter({
     abi: tankGameABI,
     strict: true,
     fromBlock: BigInt(fromBlock),
-    address: tankGameAddress[chainId as keyof typeof tankGameAddress],
+    address: address,
   });
   return await publicClient.getFilterLogs({
     filter,
--- server/routes/test.ts
+++ server/routes/test.ts
@@ -4,7 +4,6 @@ import { exec } from "child_process";
 var testRouter = Router();
 
 testRouter.post("/init", initHandler);
-testRouter.get("/kill", killHandler);
 
 function initHandler(req: Request, res: Response, next: NextFunction) {
   exec(
@@ -18,8 +17,7 @@ function initHandler(req: Request, res: Response, next: NextFunction) {
       console.log("anvil instance launched");
     }
   );
-  console.log("admin address: " + req.body.adminAddress);
-  // exec(`export ADMIN_ADDRESS=${req.body.adminAddress}`);
+  console.log("admin address " + req.body.adminAddress);
   // deploy the contracts for sim
   exec(
     `cd ../contracts && export ADMIN_ADDRESS=${req.body.adminAddress} && forge script script/TankGameDeployerSim.s.sol --broadcast --rpc-url http://0.0.0.0:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`,
@@ -29,23 +27,22 @@ function initHandler(req: Request, res: Response, next: NextFunction) {
         console.error(`Error: ${error.message}`);
         return res.status(500).send(`Error: ${error.message}`);
       }
-      const result = stdout
-        .split("\n")
-        .filter((x) => x.includes("at address:"))
-        .map((line) => {
-          const match =
-            line.match(/(\w+)\s+at\s+address:\s+(0x[a-fA-F0-9]+)/) || [];
-          return {
-            name: match[1],
-            address: match[2],
-          };
-        });
-
+      const result = parseAddresses(stdout);
       return res.send(result);
     }
   );
 }
-function killHandler(req: Request, res: Response, next: NextFunction) {
-  res.send("kill the simulation");
+
+function parseAddresses(stdout: string) {
+  let result: { [key: string]: string } = {};
+  stdout
+    .split("\n")
+    .filter((x) => x.includes("at address:"))
+    .forEach((line) => {
+      const match =
+        line.match(/(\w+)\s+at\s+address:\s+(0x[a-fA-F0-9]+)/) || [];
+      result[match[1]] = match[2];
+    });
+  return result;
 }
 export default testRouter;
