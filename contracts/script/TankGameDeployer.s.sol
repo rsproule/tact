5ffc635fde411ea3dab0cf31f86463cc3824012b
rsproule <rsproule23@gmail.com>
2023-10-20 14:23:02 -0700

    fix: vscode config and removing unused imports

--- contracts/script/TankGameDeployer.s.sol
+++ contracts/script/TankGameDeployer.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 import { Script, console } from "forge-std/Script.sol";
-// import { TankGameFactory } from "src/base/TankGameFactory.sol";
 import { TankGame } from "src/base/TankGameV2.sol";
 import { ITankGame } from "src/interfaces/ITankGame.sol";
 import { GameView } from "src/view/GameView.sol";
