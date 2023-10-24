8a0c5c31b953c06a83d483f82997e7207a67d771
rsproule <rsproule23@gmail.com>
2023-10-24 10:16:16 -0700

    feat: bring back the factory

--- contracts/src/base/TankGameFactory.sol
+++ contracts/src/base/TankGameFactory.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// import { TankGame } from "src/base/TankGameV2.sol";
-// import { ITankGame } from "src/interfaces/ITankGame.sol";
+import { TankGame } from "src/base/TankGameV2.sol";
+import { ITankGame } from "src/interfaces/ITankGame.sol";
 
-// contract TankGameFactory {
-//     event GameCreated(address game, ITankGame.GameSettings settings);
+contract TankGameFactory {
+    event GameCreated(address game, ITankGame.GameSettings settings);
 
-//     function createGame(ITankGame.GameSettings calldata settings, address _owner) external returns (TankGame game) {
-//         game = new TankGame(settings, _owner);
-//         address gameAddress = address(game);
-//         emit GameCreated(gameAddress, settings);
-//     }
-// }
+    function createGame(ITankGame.GameSettings calldata settings, address _owner) external returns (TankGame game) {
+        game = new TankGame(settings, _owner);
+        address gameAddress = address(game);
+        emit GameCreated(gameAddress, settings);
+    }
+}
