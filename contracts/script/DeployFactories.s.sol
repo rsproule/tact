2a81c72fd513d5ad2354790e61ece63fcff6bfc0
rsproule <rsproule23@gmail.com>
2023-10-24 10:15:09 -0700

    feat: factory deployer script

---
+++ contracts/script/DeployFactories.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Script, console } from "forge-std/Script.sol";
+import { TankGameFactory } from "src/base/TankGameFactory.sol";
+import { HookFactory } from "src/base/HookFactory.sol";
+
+contract DeployTankGameFactories is Script {
+    function run() public {
+        vm.startBroadcast();
+        console.log("TankGameFactory deployed at address: %s", address(new TankGameFactory()));
+        console.log("HookFactory deployed at address: %s", address(new HookFactory()));
+    }
+}
